60a7b969e64b104b512688546d8e575b9af462c4
AQadir64 <a.qadir67@outlook.com>
2022-07-20 15:44:04 +0500

    feat: typescript groth16 and plonk template

---
+++ template/typescript/groth16/util/index.ts
@@ -0,0 +1,47 @@
+import { BigNumberish } from 'ethers';
+import path from 'path';
+// @ts-ignore
+import { groth16 } from 'snarkjs';
+
+export type Witness = {
+  a: number;
+  b: number;
+  c: number;
+};
+
+export type ProveProps = {
+  solProof: Proof;
+  publicSignals: Array<string>;
+};
+
+export type Proof = {
+  a: [BigNumberish, BigNumberish];
+  b: [[BigNumberish, BigNumberish], [BigNumberish, BigNumberish]];
+  c: [BigNumberish, BigNumberish];
+};
+
+export const prove = async (witness: Witness): Promise<ProveProps> => {
+  const wasmPath = path.join(
+    __dirname,
+    '../circuits/build/Multiplier/Multiplier_js/Multiplier.wasm'
+  );
+  const zkeyPath = path.join(
+    __dirname,
+    '../circuits/build/Multiplier/circuit_final.zkey'
+  );
+  const { proof, publicSignals } = await groth16.fullProve(
+    witness,
+    wasmPath,
+    zkeyPath
+  );
+
+  const solProof: Proof = {
+    a: [proof.pi_a[0], proof.pi_a[1]],
+    b: [
+      [proof.pi_b[0][1], proof.pi_b[0][0]],
+      [proof.pi_b[1][1], proof.pi_b[1][0]],
+    ],
+    c: [proof.pi_c[0], proof.pi_c[1]],
+  };
+  return { solProof, publicSignals };
+};
---
+++ template/typescript/plonk/scripts/deploy.ts
@@ -0,0 +1,22 @@
+import { ethers } from 'hardhat';
+
+async function main() {
+  const Verifier = await ethers.getContractFactory('PlonkVerifier');
+  const verifier = await Verifier.deploy();
+
+  console.log('Verifier deployed to:', verifier.address);
+
+  const Multiplier = await ethers.getContractFactory('Multiplier');
+
+  const multiplier = await Multiplier.deploy(verifier.address);
+
+  await multiplier.deployed();
+  console.log('Multiplier deployed to:', multiplier.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
