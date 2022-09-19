8aaeea27335b9a8e8ee158612b63cdaf6fb3e0c5
sleepyqadir <a.qadir67@outlook.com>
2022-09-19 12:57:08 +0500

    chore(templates): config base url change and updated circom_tester version

--- templates/config/shield.config.js
+++ templates/config/shield.config.js
@@ -2,10 +2,10 @@ module.exports = {
   // (optional) solidity version for compiled contracts, defaults to `^0.8.0`
   solidity: "^0.8.0",
   circom: {
-    // (optional) Base path for files being read, defaults to `./circuits/`
-    inputBasePath: "./circuits/",
-    // (optional) Base path for files being output, defaults to `./circuits/`
-    outputBasePath: "./build/",
+    // (optional) Base path for files being read, defaults to `/circuits`
+    inputBasePath: "/circuits",
+    // (optional) Base path for files being output, defaults to `/build`
+    outputBasePath: "/build",
     // (required) The final ptau file, relative to inputBasePath, from a Phase 1 ceremony
     ptau: "powersOfTau28_hez_final_10.ptau",
     // (required) Each object in this array refers to a separate circuit
@@ -13,11 +13,11 @@ module.exports = {
       {
         // (required) The name of the circuit
         name: "demo",
-        // (optional) Protocol used to build circuits ("groth16" or "plonk"), defaults to "groth16"
+        // (required) Protocol used to build circuits ("groth16" or "plonk"), defaults to "groth16"
         protocol: "groth16",
-        // (optional) Input path for circuit file, inferred from `name` if unspecified
+        // (required) Input path for circuit file, inferred from `name` if unspecified
         circuit: "demo.circom",
-        // (optional) Output path for zkey file, inferred from `name` if unspecified
+        // (required) Output path for zkey file, inferred from `name` if unspecified
         zkey: "demo.zkey",
       },
     ],
